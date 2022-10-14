92cae84040676be7e51d8fa1608dc52b1b16e4bf
Guilherme Marques <gugosin3@gmail.com>
2022-10-14 17:37:35 -0300

    add de js

---
+++ condição/script.js
@@ -0,0 +1,12 @@
+// exemplo da carteira de habilitação
+
+var idade = 25
+var idadeMinima = 18;
+
+console.log('antes do if')
+if(idade>=idadeMinima){
+    console.log("pode tirar a carteira")
+}
+if(idade<idadeMinima){
+    console.log("ainda não pode tirar a carteira")
+}
