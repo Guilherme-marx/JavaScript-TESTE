7df0036cedb02f5a298ea429946f1424f17a3917
Guilherme Marques <gugosin3@gmail.com>
2022-10-14 17:11:00 -0300

    add no js

---
+++ arrays/script.js
@@ -0,0 +1,12 @@
+// nas arr o cod começa em 0 não em 1
+var arr = [1,2,3,4,5,6]
+/*contagem[0,1,2,3,4,5]*/
+/* nas arrays não possuem "especificações" são apenas obj
+servem como lista*/
+var arr = [5, 'marcos', true, {teste1}]
+
+console.log(arr);
+
+arr[4] =10;
+
+console.log(arr);
