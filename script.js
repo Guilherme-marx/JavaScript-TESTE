a879c7c50879019aed14461417eb87217df81f31
Guilherme Marques <gugosin3@gmail.com>
2022-10-14 16:03:39 -0300

    #

---
+++ script.js
@@ -0,0 +1,12 @@
+//tst d sgr
+//number
+var numero = 5;
+console.log(numero)
+console.log(typeof numero);
+//string
+var nome ='guilherme';
+var sobrenome='marques';
+var nomeCompleto= nome + " " +sobrenome;
+console.log(nomeCompleto);
+
+//teste
