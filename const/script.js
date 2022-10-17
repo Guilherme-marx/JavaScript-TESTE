8463de7b3c470b56cdc7b3a37b45e9f759f1d68e
Guilherme Marques <gugosin3@gmail.com>
2022-10-17 16:09:47 -0300

    #

--- const/script.js
+++ const/script.js
@@ -1,2 +1,18 @@
 document.getElementById('demo').style.fontSize='32px';
 
+//A palavra-chave consté um pouco enganosa. Não define um valor constante. Ele define uma referência constante a um valor.
+/* por isso não se pode
+Reatribuir um valor constante
+Reatribuir uma matriz constante
+Reatribuir um objeto constante
+
+Mas você pode:
+
+Altere os elementos da matriz constante
+Alterar as propriedades do objeto constante*/
+/*
+const cars =["saab",'volvo','bmw'];
+//pode mudar um elemento
+cars[0]='toyota';
+//ou adicionar
+cars.push('audi');*/
